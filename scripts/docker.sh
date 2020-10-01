48550a0ed190c7872debd161117319b8b8125399
davidsbond <davidsbond93@gmail.com>
2020-10-02 00:22:07 +0100

    Use GitHub container registry instead

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+REGISTRY=ghcr.io
+IMAGE=${REGISTRY}/davidsbond/homelab
 TAG=$(git describe --tags --always)
 
 docker build \
-  -t docker.pkg.github.com/davidsbond/homelab/homelab:latest \
-  -t docker.pkg.github.com/davidsbond/homelab/homelab:${TAG} .
+  -t ${IMAGE}:latest \
+  -t ${IMAGE}:${TAG} .
 
 # If env variable is set, auth with registry and push images.
 if [ "$PUSH" = "true" ]; then
-  echo ${GITHUB_TOKEN} | docker login docker.pkg.github.com -u davidsbond --password-stdin
-  docker push docker.pkg.github.com/davidsbond/homelab/homelab:latest
-  docker push docker.pkg.github.com/davidsbond/homelab/homelab:${TAG}
+  echo ${GITHUB_TOKEN} | docker login ${REGISTRY} -u davidsbond --password-stdin
+  docker push ${IMAGE}:latest
+  docker push ${IMAGE}:${TAG}
 fi
