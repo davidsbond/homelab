5716782ea29ae01b0eae94425b644bacb5cfbfa1
davidsbond <davidsbond93@gmail.com>
2020-10-30 19:20:50 +0000

    Run shellcheck on scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,14 +6,14 @@ BIN_DIR=${DIR}/bin
 # Application metadata
 APP_VERSION=$(git describe --tags --always)
 
-rm -rf ${BIN_DIR}
-mkdir -p ${BIN_DIR}
+rm -rf "${BIN_DIR}"
+mkdir -p "${BIN_DIR}"
 
 # Iterates over each subdirectory in ~/cmd that contains a main.go file
 # and builds the binary. Each binary is placed within ~/bin.
-for dir in $(find ${DIR}/cmd -name 'main.go' -print0 | xargs -0 -n1 dirname | sort | uniq); do
-  APP_NAME=$(basename ${dir})
-  APP_DESCRIPTION=$(cat ${dir}/DESCRIPTION)
+for dir in $(find "${DIR}"/cmd -name 'main.go' -print0 | xargs -0 -n1 dirname | sort | uniq); do
+  APP_NAME=$(basename "${dir}")
+  APP_DESCRIPTION=$(cat "${dir}"/DESCRIPTION)
   COMPILED=$(date +%s)
 
   OUTPUT=${BIN_DIR}/${APP_NAME}
@@ -23,6 +23,6 @@ for dir in $(find ${DIR}/cmd -name 'main.go' -print0 | xargs -0 -n1 dirname | so
 "-X 'pkg.dsb.dev/environment.compiled=${COMPILED}'"\
 "-X 'pkg.dsb.dev/environment.ApplicationName=${APP_NAME}'"\
 "-X 'pkg.dsb.dev/environment.ApplicationDescription=${APP_DESCRIPTION}'" \
-    -o ${OUTPUT} ${dir}
+    -o "${OUTPUT}" "${dir}"
 done
 
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,10 +4,10 @@ REGISTRY=ghcr.io
 IMAGE=${REGISTRY}/davidsbond/homelab
 TAG=$(git describe --tags --always)
 
-echo ${GITHUB_TOKEN} | docker login ${REGISTRY} -u davidsbond --password-stdin
+echo "${GITHUB_TOKEN}" | docker login ${REGISTRY} -u davidsbond --password-stdin
 
 docker buildx create --use
 docker buildx build --platform linux/amd64,linux/arm64 \
   --push \
   -t ${IMAGE}:latest \
-  -t ${IMAGE}:${TAG} .
+  -t ${IMAGE}:"${TAG}" .
