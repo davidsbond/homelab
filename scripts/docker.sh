c4281e613d87713c5a9e9e4804d966901ac9622a
David Bond <davidsbond@users.noreply.github.com>
2020-11-26 17:05:49 +0000

    Skip docker build if no go files have changed since last commit (#53)

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+CHANGES=$(git diff --name-only HEAD HEAD~1 | grep .go)
+if [ -z "$CHANGES" ]
+then
+  echo "No go files have changed, skipping docker build"
+  exit
+fi
+
 REGISTRY=ghcr.io
 IMAGE=${REGISTRY}/davidsbond/homelab
 TAG=$(git describe --tags --always)
