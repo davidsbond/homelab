f24cf0a709f7cf77375c38170fc87291433db942
David Bond <davidsbond@users.noreply.github.com>
2020-12-30 12:08:34 +0000

    Add DNS records for all k3s nodes (#115)
    Updates cloudflare DNS records to use all tailscale ips for the k3s cluster to route
    from `*.homelab`. Also moves directory ingress to `directory.homelab.dsb.dev`, so I
    don't need the extra `homelab` dns record.

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -7,10 +7,34 @@ data "cloudflare_zones" "dsb_dev" {
   filter {}
 }
 
-resource "cloudflare_record" "homelab" {
+resource "cloudflare_record" "homelab_0" {
   zone_id = lookup(data.cloudflare_zones.dsb_dev.zones[0], "id")
   name    = "*.homelab"
-  value   = var.homelab_ip
+  value   = var.homelab_0_ip
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "homelab_1" {
+  zone_id = lookup(data.cloudflare_zones.dsb_dev.zones[0], "id")
+  name    = "*.homelab"
+  value   = var.homelab_1_ip
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "homelab_2" {
+  zone_id = lookup(data.cloudflare_zones.dsb_dev.zones[0], "id")
+  name    = "*.homelab"
+  value   = var.homelab_2_ip
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "homelab_3" {
+  zone_id = lookup(data.cloudflare_zones.dsb_dev.zones[0], "id")
+  name    = "*.homelab"
+  value   = var.homelab_3_ip
   type    = "A"
   ttl     = 3600
 }
