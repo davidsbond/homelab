37a660b52a6d84ac21889a261858c65216c82b60
David Bond <davidsbond@users.noreply.github.com>
2021-03-16 01:19:40 +0000

    Second attempt to get upx working (#194)
    Switches to the debian based golang images, installs and runs upx.

--- scripts/node_envs.sh
+++ scripts/node_envs.sh
@@ -1,2 +1,7 @@
-export TAILSCALE_ADDR=$(tailscale status -json | jq -r .Self.TailAddr)
-export K3S_NODE_NAME=k3s-$(echo $TAILSCALE_ADDR | tr . -)
+#!/usr/bin/env bash
+
+TAILSCALE_ADDR=$(tailscale status -json | jq -r .Self.TailAddr)
+K3S_NODE_NAME=k3s-$(echo "$TAILSCALE_ADDR" | tr . -)
+
+export TAILSCALE_ADDR
+export K3S_NODE_NAME
